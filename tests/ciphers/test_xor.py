bc8a236caf2d327c08c5115097ffd9730366e2c9
tim-othy <timothymehay@gmail.com>
2020-07-18 16:07:01 +0200

    Updated repeating key xor test

--- tests/ciphers/test_xor.py
+++ tests/ciphers/test_xor.py
@@ -12,8 +12,9 @@ class TestXor(TestCase):
 
         self.assertEqual(HexEncoder.encode(Xor.encrypt(hex1, hex2, HexEncoder)), target)
 
-    def test_xor_key_different_length(self):
-        key = "a"
-        text = "abcdefg"
+    def test_repeating_key_xor(self):
+        plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+        key = "ICE"
+        target="0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
-        self.assertEqual(Xor.encrypt(key, text), "\x00\x03\x02\x05\x04\x07\x06")
+        self.assertEqual(HexEncoder.encode(Xor.encrypt(key, plaintext)), target)
