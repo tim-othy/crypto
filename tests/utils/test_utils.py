a08a28c9f8229d458e04a09b85bbcb1ce677dcbc
tim-othy <timothymehay@gmail.com>
2020-07-20 21:11:42 +0200

    Moved get_fixture_path to utils; added util to read file in chunks

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,6 +1,7 @@
+import os
 from unittest import TestCase
 
-from utils.utils import get_intersection
+from utils.utils import get_intersection, get_fixtures_path, read_file_chunks
 
 
 class TestUtils(TestCase):
@@ -8,3 +9,12 @@ class TestUtils(TestCase):
         source = {'a': 1, 'b': 2, 'c': 3}
         target = list(source.keys())
         self.assertEqual(get_intersection(source, source), target)
+
+    def test_read_file_chunks(self):
+        with open(os.path.join(get_fixtures_path(), "small_file.txt"), "r") as file:
+            chunk_size = 2
+            source = read_file_chunks(file, chunk_size=chunk_size)
+            test_string = "This is intended to test reading files in chunks."
+            target = (test_string[i: i+chunk_size] for i in range(0, len(test_string), chunk_size))
+
+            self.assertEqual(list(source), list(target))
--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,19 @@
+import os
+from typing import Generator, TextIO
+
+
 def get_intersection(dict1: dict, dict2: dict) -> list:
     return [key for key in dict1 if key in dict2]
+
+
+def get_fixtures_path() -> str:
+    return os.path.join(os.getcwd(), "tests", "fixtures")
+
+
+def read_file_chunks(file: TextIO, chunk_size: int) -> Generator:
+    while True:
+        chunk = file.read(chunk_size)
+        if not chunk:
+            break
+        else:
+            yield chunk
