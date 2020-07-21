4d3899a71ce181c7ba5fb724d5ab63ce0cf557f4
tim-othy <timothymehay@gmail.com>
2020-07-21 18:57:52 +0200

    Added utils

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,20 +1,37 @@
 import os
 from unittest import TestCase
 
-from utils.utils import get_intersection, get_fixtures_path, read_file_chunks
+import utils.utils as utils
 
 
 class TestUtils(TestCase):
     def test_get_intersection_dictionary_with_itself(self):
         source = {'a': 1, 'b': 2, 'c': 3}
         target = list(source.keys())
-        self.assertEqual(get_intersection(source, source), target)
+        self.assertEqual(utils.get_intersection(source, source), target)
 
     def test_read_file_chunks(self):
-        with open(os.path.join(get_fixtures_path(), "small_file.txt"), "r") as file:
+        with open(os.path.join(utils.get_fixtures_path(), "small_file.txt"), "r") as file:
             chunk_size = 2
-            source = read_file_chunks(file, chunk_size=chunk_size)
+            source = utils.read_file_chunks(file, chunk_size=chunk_size)
             test_string = "This is intended to test reading files in chunks."
             target = (test_string[i: i+chunk_size] for i in range(0, len(test_string), chunk_size))
 
             self.assertEqual(list(source), list(target))
+
+    def test_transpose_blocks(self):
+        source = ["1234", "5678", "abcd", "efgh"]
+        target = ["15ae", "26bf", "37cg", "48dh"]
+        self.assertEqual(utils.transpose_blocks(source), target)
+
+    def test_get_number_partitions(self):
+        source = "This is a string"
+        chunk_size = 3
+        target = 6
+        self.assertEqual(utils.get_number_partitions(source, chunk_size), target)
+
+    def test_get_partitions(self):
+        source = "This is a string"
+        chunk_size = 3
+        target = ["Thi", "s i", "s a", " st", "rin", "g"]
+        self.assertEqual(list(utils.partition_string(source, chunk_size)), target)
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,8 @@
 import os
-from typing import Generator, TextIO
+from itertools import tee
+import typing as T
+
+from numpy.ma import floor
 
 
 def get_intersection(dict1: dict, dict2: dict) -> list:
@@ -10,10 +13,28 @@ def get_fixtures_path() -> str:
     return os.path.join(os.getcwd(), "tests", "fixtures")
 
 
-def read_file_chunks(file: TextIO, chunk_size: int) -> Generator:
+def read_file_chunks(file: T.TextIO, chunk_size: int) -> T.Generator:
     while True:
         chunk = file.read(chunk_size)
         if not chunk:
             break
         else:
             yield chunk
+
+
+def partition_string(string: str, chunk_size: int) -> T.Generator:
+    return (string[i*chunk_size:(i + 1)*chunk_size] for i in range(int(get_number_partitions(string, chunk_size))))
+
+
+def get_number_partitions(string: str, chunk_size: int) -> int:
+    return floor(len(string)/chunk_size) + 1
+
+
+def pairwise(iterable: T.Iterable) -> T.Iterator[T.Tuple[T.Any, T.Any]]:
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def transpose_blocks(blocks: list) -> list:
+    return ["".join(i) for i in map(list, zip(*blocks))]
